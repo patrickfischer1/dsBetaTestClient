34105515e5d1ed93cffb69b2dfe8b8dfe6d8ca92
Olly Butters <oliver.butters@gmail.com>
2018-11-27 16:36:19 +0000

    first attempt at docs check

---
+++ checkDocumentationUpdated.sh
@@ -0,0 +1,23 @@
+# Check if the documentation in the man directory matches what should be in it
+# based on what is in the headers of the R scripts.
+
+# Concatenate all the files in the man dir into one long string and md5sum it.
+orig_sum=$(find man -type f | sort -u | xargs cat | md5sum)
+
+# Rebuild the documentation.
+R -e "devtools::document()" 2&> /dev/null
+
+# Concatenate all the files in the man dir into one long string and md5sum it.
+new_sum=$(find man -type f | sort -u | xargs cat | md5sum)
+
+# echo $orig_sum
+# echo $new_sum
+
+if [ "$orig_sum" != "$new_sum" ]; then
+  echo "Your committed manual files (man/*.Rd) are out of sync with the documentation in the R files."
+  echo "Run roxygenise() locally then commit again."
+  exit 1
+else
+  echo "Documentation up to date."
+  exit 0
+fi
