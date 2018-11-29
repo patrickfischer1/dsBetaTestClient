9199803731ac3fdfbf1a14fd3f629cf6e0e5780e
Stuart Wheater <stuart.wheater@arjuna.com>
2018-11-29 15:03:53 -0800

    Reworked 'opal.login' call

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -32,8 +32,8 @@ password <- c(getOption("opal.password"), getOption("opal.password"), getOption(
 table <- c("CNSIM.CNSIM1", "CNSIM.CNSIM2", "CNSIM.CNSIM3")
 logindata <- data.frame(server,url,user,password,table)
 
-adminopals <- opal.login(username=user,password=password,url=url)
-opaladmin::dsadmin.install_package(opal=adminopals,pkg="dsBetaTest",githubusername="datashield",ref="master")
+adminopals <- opal.login(username=getOption("opal.username"),password=getOption("opal.password"),url=getOption("opal.url"))
+opaladmin::dsadmin.install_package(opal=adminopals,pkg="dsBetaTest",githubusername="StuartWheater",ref="master")
 opal.logout(adminopals)
 
 opals <- opal::datashield.login(logins=logindata,assign=TRUE,variables=getOption("datashield.variables", NULL))
