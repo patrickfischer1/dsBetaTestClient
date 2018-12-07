2af93108bcc90d4a7cbe4f9ce076a49a9aa9b451
Stuart Wheater <stuart.wheater@arjuna.com>
2018-12-07 05:53:30 -0800

    Fixed typo

--- tests/testthat/test-ds.glm.o.R
+++ tests/testthat/test-ds.glm.o.R
@@ -26,10 +26,10 @@ source("setup.R")
 
 context("dsBetaTestClient::ds.glm.o(): Standard Poisson regression model for piecewise exponential regression analysis")
 
-mod.D<-ds.glm.o("survtime~1+time.id.f+female+age.60",family="poisson",offset="log.surv")
+mod.D<-ds.glm.o("survtime~1+time.id+female+age.60",family="poisson",offset="log.surv")
 output.D<-c(mod.D$coefficients[,1],mod.D$coefficients[,2])
 
-mod.R<-glm(EVENT~1+TID.F+FEMALE+AGE.60,family="poisson",offset=LOG.SURV)
+mod.R<-glm(SURVTIME~1+TIME.ID+FEMALE+AGE.60,family="poisson",offset=LOG.SURV)
 output.R<-c(summary(mod.R)$coefficients[,1],summary(mod.R)$coefficients[,2])
 
 test_that("glm_poisson", {
