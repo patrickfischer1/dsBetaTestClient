ae82b8b7c74661d28eb8b28f94be0ad47cd0e740
Stuart Wheater <stuart.wheater@arjuna.com>
2019-03-21 16:02:28 +0000

    Fix missing exports

--- R/ds.listOpals.o.R
+++ R/ds.listOpals.o.R
@@ -14,6 +14,8 @@
 #' @return Lists all of the sets of Opals currently found in the analytic environment and advises
 #' the user how best to respond depending whether there are zero, one or multiple Opals detected.
 #' @author Burton, PR. 28/9/16
+#' @export
+#'
 
 ds.listOpals.o<-function(){
 
