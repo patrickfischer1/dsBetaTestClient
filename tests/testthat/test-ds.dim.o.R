327c976fd8a2f2228a3c06a8ad931be5df39a463
Stuart Wheater <stuart.wheater@arjuna.com>
2019-03-27 16:15:54 +0000

    'ds.dim.o' smoke test

---
+++ tests/testthat/test-ds.dim.o.R
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) 2014 OBiBa,
+#               2019 University of Newcastle upon Tyne. All rights reserved.
+#
+# This program and the accompanying materials
+# are made available under the terms of the GNU Public License v3.0.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+#
+# Set up
+#
+
+context("dsBetaTestClient::ds.dim.o")
+
+options(opal.server1="sim1", opal.server2="sim2", opal.server3="sim3")
+options(opal.table1="CNSIM.CNSIM1", opal.table2="CNSIM.CNSIM2", opal.table3="CNSIM.CNSIM3")
+options(datashield.variables=list("LAB_TSC"))
+source("setup.R")
+
+#
+# Tests
+#
+
+context("dsBetaTestClient::ds.dim.o()")
+test_that("simple dim", {
+    dim.res <- ds.dim.o('D')
+
+    expect_equal(dim.res$`dimensions of D in sim1`[[1]], 2163)
+    expect_equal(dim.res$`dimensions of D in sim2`[[1]], 3088)
+    expect_equal(dim.res$`dimensions of D in sim3`[[1]], 4128)
+    expect_equal(dim.res$`dimensions of D in combined studies`[[1]], 9379)
+})
+
+#
+# Tear down
+#
+
+source("teardown.R")
+
+context("dsBetaTestClient::ds.dim.o done")
